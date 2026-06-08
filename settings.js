5261691a0194df3ff4fc6fb55d7d7ac05e698e31
mugishaprosper758-dot <mugishaprosper758@gmail.com>
2026-06-08 22:14:17 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'IANENIGMA MD BOT',
-  author: 'IANENIGMA',
-  botName: "IANENIGMA MD BOT",
-  botOwner: 'IANENIGMA', // Your name
-  ownerNumber: process.env.OWNER_NUMBER || '256775063416', //Set your number here without + symbol
+  packname: 'PROSPER MD BOT',
+  author: 'PROSPER',
+  botName: "PROSPER MD BOT",
+  botOwner: 'PROSPER', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '256774411462', //Set your number here without + symbol
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "v4.0.0",
-  updateZipUrl: "https://github.com/mruniquehacker/IANENIGMA MD-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/mruniquehacker/PROSPER MD-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
